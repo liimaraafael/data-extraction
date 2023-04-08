070d363648015481b188041be1ca6f7681247ef2
Rafael Lima <liimaraafael@gmail.com>
2023-04-08 11:48:34 -0300

    adding new features in MySQLWriteDB.py

--- MySQLWriteDB.py
+++ MySQLWriteDB.py
@@ -1,19 +1,24 @@
-import pandas as pd
-
 from MySQL import connect_AWS
 from constant import list_write
+from functions import *
 
 engine = connect_AWS()
 
 for name in list_write:
     # Load csv
-    df = pd.read_csv("treated/" + name, sep=";")
-    df = df.drop(columns=["Unnamed: 23"])
-    df["HR_MEDICAO"] = df["HR_MEDICAO"].apply(lambda x: int(x.replace(":", "")[0:4])) / 100.
-    df = df.set_index('DT_MEDICAO')
+    df = pd.read_csv("treated/" + name, sep=";")\
+        .transform(filter_temp) \
+        .transform(filter_pto) \
+        .transform(filter_press) \
+        .transform(filter_prec) \
+        .transform(filter_radia) \
+        .transform(filter_umid) \
+        .transform(filter_ven) \
+        .transform(treat_df) \
+        .set_index("DT_MEDICAO") \
 
     # Save (AWS)
-    df.to_sql("row_data_br", con=engine, if_exists='append')
+    df.to_sql("processed_data_br", con=engine, if_exists='append')
 
     # Interaction
     print(name + " OK!\n")
