b3d60d9dc6eef4cdf076a8fcb51ad6d7547db6d2
Rafael Lima <liimaraafael@gmail.com>
2023-04-08 21:29:21 -0300

    fixing

--- MySQLWriteDB.py
+++ MySQLWriteDB.py
@@ -1,12 +1,13 @@
 from MySQL import connect_AWS
-from constant import list_write
+from constant import list_write_rs
 from functions import *
 
 engine = connect_AWS()
 
-for name in list_write:
+i = 0
+for name in list_write_rs:
     # Load csv
-    df = pd.read_csv("treated/" + name, sep=";")\
+    df = pd.read_csv("treated/" + name, sep=";") \
         .transform(filter_temp) \
         .transform(filter_pto) \
         .transform(filter_press) \
@@ -18,10 +19,17 @@ for name in list_write:
         .set_index("DT_MEDICAO") \
 
     # Save (AWS)
-    df.to_sql("processed_data_br", con=engine, if_exists='append')
+    df.to_sql("processed_data_rs", con=engine, if_exists='append')
 
     # Interaction
-    print(name + " OK!\n")
+    status = round((i / float(len(list_write_rs))) * 100, 2)
+
+    if status < 100.00:
+        print("File: " + name + " is OK!\nStatus: " + str(status) + "% complete. Just a moment, please.\n")
+
+    else:
+        print("File: " + name + " is OK!\nStatus: " + str(status) + "% complete.\n")
+    i += 1
 
 print("Success")
 print("-" * 60)
